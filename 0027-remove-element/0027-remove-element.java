4cc53311fa44570d7fd583730d2a82e87ab5fc2c
Vasanti Bendre <121009809+vasantibendre06@users.noreply.github.com>
2025-06-21 12:28:33 +0530

    Time: 0 ms (100%), Space: 41.8 MB (82.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        int n = nums.length;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
